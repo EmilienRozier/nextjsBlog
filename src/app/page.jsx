e7b2dd222f15c4e143c88c8d24f017d38a467d3b
EmilienRozier <emilienrozier@icloud.com>
2023-09-11 22:53:23 +0200

    components & context

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import CardList from "./components/cardList/CardList";
+import Featured from "./components/featured/Featured";
+import CategoryList from "./components/categoryList/CategoryList";
+import Menu from "./components/menu/Menu";
 
 export default function Home() {
-  return <div></div>;
+  return <div className={styles.container}>
+    <Featured/>
+    <CategoryList/>
+    <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+    </div>
+  </div>;
 }
